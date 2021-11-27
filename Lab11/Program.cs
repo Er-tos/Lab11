88e014c7a56fb608f4fa16e6f26c5cb9c4090b6d
a.v.ershov <a.v.ershov@137_1>
2021-11-27 10:21:11 +0700

    Добавьте файлы проекта.

---
+++ Lab11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Разработаем структуру для решения линейного уравнения 0 = kx + b.");
+            linearEquation linearEquation;
+            Console.WriteLine("Введите коэффициент уравнения k");
+            linearEquation.k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите коэффициент уравнения b");
+            linearEquation.b = Convert.ToDouble(Console.ReadLine());
+            double decision = linearEquation.Root();
+            Console.WriteLine("Решение уравнения: x = {0}", decision);
+            Console.ReadKey();
+        }
+        struct linearEquation
+        {
+            public double k;
+            public double b;
+            public double Root()
+            {
+                double x = Math.Round(- b / k, 2);
+                return x;
+            }
+        }
+    }
+}
